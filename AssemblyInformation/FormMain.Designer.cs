5d709d8f0c655e0ee7104d8702c3453d8973bba3
utilitytech278 <utilitytech278@protonmail.com>
2024-03-10 22:58:46 -0400

    FormMain: Committing changes the Designer insists upon.

--- AssemblyInformation/FormMain.Designer.cs
+++ AssemblyInformation/FormMain.Designer.cs
@@ -271,7 +271,7 @@
             this.tabPage2.Location = new System.Drawing.Point(4, 22);
             this.tabPage2.Name = "tabPage2";
             this.tabPage2.Padding = new System.Windows.Forms.Padding(3);
-            this.tabPage2.Size = new System.Drawing.Size(665, 211);
+            this.tabPage2.Size = new System.Drawing.Size(633, 210);
             this.tabPage2.TabIndex = 1;
             this.tabPage2.Text = "All Direct & Indirect References";
             this.tabPage2.UseVisualStyleBackColor = true;
@@ -299,7 +299,7 @@
             this.tabPage3.Location = new System.Drawing.Point(4, 22);
             this.tabPage3.Name = "tabPage3";
             this.tabPage3.Padding = new System.Windows.Forms.Padding(3);
-            this.tabPage3.Size = new System.Drawing.Size(665, 211);
+            this.tabPage3.Size = new System.Drawing.Size(633, 210);
             this.tabPage3.TabIndex = 2;
             this.tabPage3.Text = "Referring Assemblies";
             this.tabPage3.UseVisualStyleBackColor = true;
